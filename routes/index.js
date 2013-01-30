fde21bcd50a073d4736908d383c6459cdddb3ec1
Jimmy Jacobson <jimmy@wedgies.com>
2013-01-29 18:38:41 -0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.contact = function(req, res) {
+	res.render('contact', {title: 'Contact'});
+};
+
+exports.handleContactForm = function(req, res) {
+	console.log(req);
+	res.write('thanks');
+	res.end();
+};
